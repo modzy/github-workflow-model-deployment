e618c16bf697561ec45c2e5f039c47ea15a1d36f
bmunday3 <bwmunday@gmail.com>
2023-10-11 18:29:11 -0400

    Added deploy script to CI workflow

---
+++ deploy.py
@@ -0,0 +1,21 @@
+import os
+import json
+from modzy import ApiClient
+
+# load metadata config file
+with open("model_info.json", "r") as model_file:
+    model_info = json.load(model_file)
+
+# instantiate Modzy API Client
+client = ApiClient(os.getenv("MODZY_URL"), os.getenv("MODZY_API_KEY"))
+
+print(f"Deploying model to {os.getenv('MODZY_URL')}")
+# deploy model
+new_model_data = client.models.deploy(
+    container_image=os.getenv("CONTAINER"),
+    model_name=model_info["name"],
+    model_version=model_info["version"],
+    sample_input_file=model_info["sampleDataFilePath"]
+)
+
+print(new_model_data)    
